1ec30081fdb22ed0a163cf48a5002d10fcedc544
Liam-Guthrie <130588895+Liam-Guthrie@users.noreply.github.com>
2023-06-12 13:57:20 -0400

    Created Dragon class
    Dragon should be able to follow Player

---
+++ Dungeon-and-Dragon/Dragon.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Dragon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dragon extends Actor
+{
+    /**
+     * Act - do whatever the Dragon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        boolean dragon = true;
+        int health = 15;
+        int deltaX = 5;
+        int deltaY = 5;
+       
+        
+        java.util.List<Player> playerInLevel = getWorld().getObjects(Player.class);
+        if (!playerInLevel.isEmpty()){
+            Player player = playerInLevel.get(0);
+            moveToPlayer(player);
+        }
+    }
+}
