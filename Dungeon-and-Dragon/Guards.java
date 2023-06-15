b33b48e7fc14b706032e8621572499cc473e3fa7
Liam-Guthrie <130588895+Liam-Guthrie@users.noreply.github.com>
2023-06-15 14:20:05 -0400

    Fixed Guards

--- Dungeon-and-Dragon/Guards.java
+++ Dungeon-and-Dragon/Guards.java
@@ -12,44 +12,65 @@ public class Guards extends Actor
      int health = 10;
      int deltaX = 0;
      int deltaY = 0;
+     int speed = 1;
      boolean goingDown = true;
     /**
      * Act - do whatever the Guards wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act(){
-       
-        
-        guardMove(goingDown, guards);
-        
-            
-        // >= getWorld().getHeight() - 10
-           
-            
-        }
-        public boolean guardMove(boolean goingDown, boolean guards){
-
-  if(goingDown){
+       if (guards == true){
+        if(goingDown){
+                setLocation(getX() + deltaX, getY() + deltaY + 10);
+                deltaY = speed;
                 
                 if (getY() >= getWorld().getHeight() - 10){
                 
                 goingDown = false;                
                 }
                 //setLocation(getX(), getY() + 10);
-                deltaY = 5;
+                //deltaY = 5;
+                
             }
   else if(!goingDown){
+                setLocation(getX() + deltaX, getY() + deltaY - 10);
+                deltaY = -speed;
                 
-                if (getY() < getWorld().getHeight() + 10){
+                if (getY() < 10){
                     
                     goingDown = true;
                 }
                 //setLocation(getX(), getY() - 10);
-                deltaY = -5;
+                
             }
     
-    setLocation(getX()+deltaX, getY()+deltaY);
-    return goingDown;
+            
+        // >= getWorld().getHeight() - 10
+           
+    } 
+    
+    if (health == 0){
+        guards = false;
+    }
+    else if (health == 10){
+        guards = true;
+    }
+    else if (health < 10){
+        guards = true;
+    }
+    
+    
+        
+        
+
+        
 }
+public void playerAttack(){
+        if(player.getX() <= player.speed*2){
+            if(Greenfoot.mouseClicked(this)){
+                this.health-=player.damage;
+            }
+        }
+    }
 }
 
