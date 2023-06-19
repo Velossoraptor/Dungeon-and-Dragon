4e04e365c0441cc91755405084eaa502070b8c66
Velossoraptor <130588823+Velossoraptor@users.noreply.github.com>
2023-06-19 11:57:26 -0400

    Damage
    Player and ememies can damage eachother

--- Dungeon-and-Dragon/Dragon.java
+++ Dungeon-and-Dragon/Dragon.java
@@ -10,50 +10,82 @@ public class Dragon extends Actor
 
 {
     boolean dragon = true;
-        int health = 15;
-        int deltaX = 0;
-        int deltaY = 0;
-        int speed = 3;
-        GreenfootImage dragonImage = new GreenfootImage("Dragon.png");
+    int health = 15;
+    int deltaX = 0;
+    int deltaY = 0;
+    int speed = 1;
+    GreenfootImage dragonImage = new GreenfootImage("Dragon.png");
     /**
      * Act - do whatever the Dragon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-      setImage(dragonImage);  
-       
+        setImage(dragonImage);  
+        if(health>0){
+            moveDragon();
+        }
         
-    //java.util.List<Player> playerInLevel = getWorld().getObjects(Player.class);
-        if (dragon == true){
+        playerAttack();
+        hitPlayer();
+        //java.util.List<Player> playerInLevel = getWorld().getObjects(Player.class);
 
-        if(this.getX() < Player.getX()){
-            deltaX = speed;
+        if (!dragon){
+            setLocation(0, 0);
         }
-        else if (this.getX() > Player.getX()){
-            deltaX = -speed;
+
+        if (health <= 0){
+            dragon = false;
         }
-        if(this.getY() < Player.getY()){
-            deltaY = speed;
+
+    }
+    public void moveDragon(){
+        boolean touchingWall = this.isTouching(Wall.class)||this.isTouching(Wall2.class) || this.isTouching(Pillar.class);
+
+        //if(!touchingWall){
+            if (dragon == true){
+
+                if(this.getX() < getWorldOfType(Level3.class).getPlayer().getX()){
+                    deltaX = speed;
+                }
+                else if (this.getX() > getWorldOfType(Level3.class).getPlayer().getX()){
+                    deltaX = -speed;
+                }
+                if(this.getY() < getWorldOfType(Level3.class).getPlayer().getY()){
+                    deltaY = speed;
+                }
+                if(this.getY() > getWorldOfType(Level3.class).getPlayer().getY()){
+                    deltaY = -speed;
+                }
+            }           
+        //}
+        /*else if(touchingWall){
+            if(deltaY==-speed){
+                deltaY = speed;
+            }
+            if(deltaX==-speed){
+                deltaX = speed;
+            }
+            if(deltaY==speed){
+                deltaY = -speed;
+            }
+            if(deltaX==speed){
+                deltaX = -speed;
+            }
+        }*/
+        setLocation(getX() + deltaX, getY() + deltaY);
+    }
+    public void playerAttack(){
+        if(getWorldOfType(Level3.class).getPlayer().getX() <= this.getX()+300 ||getWorldOfType(Level3.class).getPlayer().getX() <= this.getX()-300){ 
+
+            if(Greenfoot.mouseClicked(this)){
+                this.health-=getWorldOfType(Level3.class).getPlayer().attack();
+            }
         }
-        if(this.getY() > Player.getY()){
-            deltaY = -speed;
+    }
+    public void hitPlayer(){
+        if(this.isTouching(Player.class)){
+            getWorldOfType(Level3.class).getPlayer().damage();
         }
-        
-        
     }
-    if (dragon == false){
-        setLocation(0, 0);
-    }
-    
-    if (health == 0){
-        dragon = false;
-    }
-    else if (health == 15){
-        dragon = true;
-    }
-    else if (health < 15){
-        dragon = true;
-    }
-}
 }
